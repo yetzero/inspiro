713feba0a218e4759c6a728b5738417695209eb4
yetzero <yetzero@gmail.com>
2015-05-13 15:01:16 -0300

    404 page

---
+++ 404.php
@@ -0,0 +1,19 @@
+<?php get_header(); ?>
+
+		<div class="page-top">
+			<img src="/media/ocean.jpg" alt="">
+			<!--<div class="darkener"></div>-->
+			<div class="page-top-content">
+				<h1>404</h1>
+			<p class="hero-subtitle">No encontrado</p>
+			</div>
+		</div>
+		<div class="page-container">
+			<section>
+				<p>No encontramos la dirección que escribió. Pruebe buscando lo que necesita.</p>
+				<p><?php get_search_form(); ?></p>
+			</section>
+		</div>
+
+
+<?php get_footer(); ?>
